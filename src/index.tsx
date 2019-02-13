97a97de2a004e292dde2655dce5c094e3804150d
umangg21 <umangg21@gmail.com>
2019-02-13 22:56:04 +0530

    fix few issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import HomeView from './component/Home';
 
 ReactDOM.render(<HomeView />, document.getElementById('root'));
